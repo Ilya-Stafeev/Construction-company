c262db29d40bf4e75bfd776ddc2af2d55985e4b1
Ilya-Stafeev <stafeev-ilya@bk.ru>
2024-02-05 03:43:04 +0700

    I made another block

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 import Header from "../components/screens/header/Header"
 import Main_blok from "../components/screens/main_block/Main-block"
+import Services from "../components/screens/services/Services"
 import Modal_cons from "../components/interfaces/modals/modal_cons/Modal_cons"
 import Modal from "../components/interfaces/modals/Modal"
 
@@ -22,7 +23,8 @@ const Home = () => {
                 <Modal_cons
                     setActive={setModalConsActive}
                 />
-            </Modal>            
+            </Modal>
+            <Services/>
             <div>Дом</div>
         </>
     )
